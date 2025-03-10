7e5e42cfcb96795a3e4cd4ed366efdcfb419537f
Yilin Xu <yilinxuhz@gmail.com>
2025-03-10 14:15:23 -0500

    Update Annotated bluesky BERTopic.ipynb and reddit_scraper.py

--- reddit/reddit_scraper.py
+++ reddit/reddit_scraper.py
@@ -2,6 +2,7 @@
 Final Project: Reddit Scraping
 Yilin Xu
 """
+# Import packages
 import praw
 import numpy as np
 import pandas as pd
@@ -12,10 +13,12 @@ import prawcore
 import time
 import random
 
+# Create Empty List to Store Information Scraped
 post_lst = []
 comment_lst = []
 pd.set_option('max_colwidth', None)
 
+# Load Authorized Reddit Instance
 reddit = praw.Reddit(
     client_id="oCPIQsxeqy9ioFGkQNv26A",
     client_secret="07TikO3G7waJyTFTXACma2gsUnsPmg",
@@ -24,29 +27,42 @@ reddit = praw.Reddit(
     username="Background-Motor-921"
 )
 
+# Set start date for contents to be scraped.
+# Any posts posted before the start data will not be scraped
 start_date = '05-01-25 00:00:00'
 start_date = datetime.datetime.strptime(start_date, '%d-%m-%y %H:%M:%S').timestamp()
 
+# Set a preliminary list of keys we wish to remove
+# These keys are removed because they are problematic or unnecessary
 key_to_remove = ['_fetched', '_reddit', '_replies',\
                  '_submission', 'body_html', '_additional_fetch_params',\
                  '_comments', '_comments_by_id', 'selftext_html']
 
 def clean_data(data, key_to_remove):
+    '''
+    Keep data with needed keys and handle missing
+    information on author and subreddit
+    '''
     data['author'] = data['author'].name if data['author'] else "Not Applicable"
     data['subreddit'] = data['subreddit'].display_name if data['subreddit'] else "Not Applicable"
     return {key: value for key, value in data.items() if key not in key_to_remove}
 
 def process_comments(cf_lst):
+    '''
+    Recursivelt process CommentForest(tree structure) and 
+    MoreComments instances to obtain complete comments structure
+    '''
     for comment in cf_lst:
-        if isinstance(comment, praw.models.MoreComments):
-            if comment.comments():
-                process_comments(comment.comments())
+        # check is the instance is MoreComment, if yes, need to expand
+        if isinstance(comment, praw.models.MoreComments): 
+            if comment.comments(): # expand MoreComments and check is not empty
+                process_comments(comment.comments()) # recursion to extract the new CommentForest list
 
-        time.sleep(1)
+        time.sleep(1) # Respect Reddit's rate limit
 
-        comment_data = clean_data(vars(comment), key_to_remove)
-        comment_data.setdefault('title', '')
-        comment_data['selftext'] = comment_data.get('body', '')
+        comment_data = clean_data(vars(comment), key_to_remove) # clean data, see helper function
+        comment_data.setdefault('title', '') #set value for title in case there is None
+        comment_data['selftext'] = comment_data.get('body', '') #save comment text value as selftext to align with post data key
 
         # Calculate upvote ratio safely
         total_votes = comment_data.get('downs', 0) + comment_data.get('ups', 0)
@@ -57,24 +73,31 @@ def process_comments(cf_lst):
         comment_data['num_crossposts'] = count
         comment_data['num_comments'] = count
 
+        # append to processed comment_lst
         comment_lst.append(comment_data)
 
 def safe_request():
-    """Fetch posts while handling rate limits"""
+    '''
+    Fetch posts while handling rate limits
+    '''
     try:
         for post in reddit.subreddit("all").search("LA wildfire", sort="top", time_filter="month", limit= 250):  # Reduce limit
             date = post.created_utc
+            # check if it posted later than indicated start date
             if date < start_date:
                 continue
             
+            # process post data with helper function
             post_data = clean_data(vars(post), key_to_remove)
             post_lst.append(post_data)
-            print("Processing Post:", post.title)
+            print("Processing Post:", post.title) # Print statement: help keep track of progress
 
+            # Extract all first level comments(directly replying to post) 
             post.comments.replace_more(limit=None)
-            time.sleep(random.uniform(1, 3))
+            time.sleep(random.uniform(1, 3)) # Respect Reddit's rate limit
 
-            if post.comments.list():
+            if post.comments.list(): # if there are comments(CommentForest object), proceed to process them
+                # Call recursive function on the list of CommentForest objects
                 process_comments(post.comments.list())
   
             time.sleep(random.uniform(1, 3))  # Respect Reddit's rate limit
@@ -83,14 +106,18 @@ def safe_request():
     except:
         print(f"Error: ({e})")
 
+# Scrape!
 safe_request()
 
-with open('post_data_v3.json', 'w') as f:
+# Save scraped posts
+with open('reddit_post_data.json', 'w') as f:
     for obj in post_lst:
         f.write(json.dumps(obj) + "\n")
 
-with open('comment_data_v3.json', 'w') as f:
+# Save scraped comments
+with open('reddit_comment_data.json', 'w') as f:
     for obj in comment_lst:
         f.write(json.dumps(obj) + "\n")
 
+# Notify
 print("Task Finished!")
